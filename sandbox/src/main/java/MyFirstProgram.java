20ee1c1833bfe472d41e24821a48cffb252239ad
LidaAst <lida.astapchuk@gmail.com>
2016-03-03 21:19:06 -0800

    Added Point class, static distance method, and method in class

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,5 +1,19 @@
  public class MyFirstProgram {
-	public static void main(String[] args){
-		System.out.println("Privet, CheBurr!");
-	}
-}
+	 public static void main(String[] args) {
+
+		 System.out.println("Privet, CheBurr!");
+
+		 Point p1 = new Point(0, 1);
+		/* p1.x = 0;
+		 p1.y = 0;*/
+		 Point p2 = new Point(2.5, 1);
+		/* p2.x = 1;
+		 p2.y = 1;*/
+		 System.out.println("Distance between two points: (" + p1.x + ", " + p1.y + ") and (" + p2.x + ", " + p2.y + ") = " + p1.distance(p2));
+	 }
+/*
+	 public static double distance(Point p1, Point p2) {
+		 double l = (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y);
+		 return Math.sqrt(l);
+	 }*/
+ }
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Lida on 3/2/2016.
+ */
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    public double distance(Point p2) {
+        double l = (p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y);
+        return Math.sqrt(l);
+    }
+
+}
