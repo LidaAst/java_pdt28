b2c826850675d5f35a0a1d243de6d99e8bc981e4
LidaAst <lida.astapchuk@gmail.com>
2016-03-13 22:11:09 -0700

    added GroupModificationTest class

---
+++ addressbook-web-tests/src/test/java/ala/pft/addressbook/tests/GroupModificationTest.java
@@ -0,0 +1,20 @@
+package ala.pft.addressbook.tests;
+
+import ala.pft.addressbook.model.GroupData;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Lida on 3/13/2016.
+ */
+public class GroupModificationTest extends TestBase {
+
+@Test
+    public void testGroupModification (){
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+    }
+}
