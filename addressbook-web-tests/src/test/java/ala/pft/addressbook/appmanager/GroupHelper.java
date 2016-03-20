c5ff3561215d932fccdd6f1bb07cf30381fab1bb
LidaAst <lida.astapchuk@gmail.com>
2016-03-19 22:46:47 -0700

    Added precondition check for GroupDelition and GroupModification tests

--- addressbook-web-tests/src/test/java/ala/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ala/pft/addressbook/appmanager/GroupHelper.java
@@ -47,4 +47,15 @@ public class GroupHelper extends HelperBase {
     public void initGroupDeletion() {
         click(By.name("delete"));
     }
+
+    public void createGroup(GroupData group) {
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupPage();
+    }
+
+    public boolean ifThereAGroup() {
+        return isElementPresent(By.name("selected[]"));
+    }
 }
--- addressbook-web-tests/src/test/java/ala/pft/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ala/pft/addressbook/tests/GroupDeletionTest.java
@@ -1,5 +1,6 @@
 package ala.pft.addressbook.tests;
 
+import ala.pft.addressbook.model.GroupData;
 import org.testng.annotations.Test;
 
 /**
@@ -10,6 +11,9 @@ public class GroupDeletionTest extends TestBase {
     @Test
     public void testGroupDeletion(){
         app.getNavigationHelper().goToGroupPage();
+        if (! app.getGroupHelper().ifThereAGroup()){
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupDeletion();
         app.getGroupHelper().returnToGroupPage();
